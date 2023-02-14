87e26836ecb5de21ce4c30648386330e14e0a28b
Ananta Karmakar <codeantu@gmail.com>
2023-02-14 21:32:41 +0530

    Create 2.2.c

---
+++ programing_exercise/2.0/2.2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main() {
+    int paise;
+    float rupee;
+    
+   
+    printf("Enetr price in rupee : ");
+    scanf("%f",&rupee);
+   
+    paise = rupee * 100;
+    
+    printf("Price in rupeee : %d ",paise);
+     
+   
+
+}
