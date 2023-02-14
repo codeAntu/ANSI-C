6673641b9eea86e8dbe1f977d04e046648b16596
Ananta Karmakar <codeantu@gmail.com>
2023-02-14 14:53:24 +0530

    Create 1.1.c

---
+++ programing_exercise/1.1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/*Over view of C */
+/*1.1 Write aprogram that will print your maling adress in the following form */
+/*name 
+ door no , Street 
+city ,pin code */
+int main()
+{
+    printf("Ananta Karmakr \n");
+    printf("100,XYZ street\n");
+    printf("abc city ,124598547");
+    
+    
+}
