def655459a6f73132be63ab46293c6bb04a7fe3b
Ananta Karmakar <codeantu@gmail.com>
2023-02-15 11:17:18 +0530

    Create 2.5.c

---
+++ programing_exercise/2.0/2.5.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main() {
+
+    printf("***LIST OF ITEMS***\n");
+    printf("Item       Price \n");
+    printf("Rice       Rs 16.75\n");
+    printf("Suger      Rs 15.00");
+}
