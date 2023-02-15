bdbbca2b33ec6ebc9470b33e41edeb759b016e64
Ananta Karmakar <codeantu@gmail.com>
2023-02-15 21:00:03 +0530

    Create 2.9.c

---
+++ programing_exercise/2.0/2.9.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+
+    typedef int markes;
+    markes math, phy , chem , bio ;
+    
+    math = 24;
+    phy  = 48;
+    chem = 53;
+    bio  = 66; 
+    
+    printf(" math  =  %d \n phy   = %d \n chem  = %d \n bio   = %d ",math , phy , chem ,bio);
+    
+}
