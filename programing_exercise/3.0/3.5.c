47cbf676fecd58734591c86dbaf4f46a76fba348
Ananta Karmakar <codeantu@gmail.com>
2023-02-17 00:03:07 +0530

    Update 3.5.c

--- programing_exercise/3.0/3.5.c
+++ programing_exercise/3.0/3.5.c
@@ -1,16 +1,22 @@
 #include<stdio.h>
 
-int main()
-{
-    int n;
-    
-    printf("Enter a number : ");
-    scanf("%d",&n);
-    
-    while(n > 0){
-    printf("%d \n",n);
-    n = n / 10;
+
+int fun(int n, int x) {
+    int t;
+    if(n>=x)
+    {
+        fun(n,x*10);
     }
-    
+    n = n % x;
+    printf("%d\n",n);
     return 0;
 }
+int main()
+{
+    int n,x=10;
+
+    n = 5678;
+    fun(n,x);
+    return 0;
+}
+
